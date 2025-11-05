cd3d24a8356207aa4744d4bf59e271ba46f1db25
PedroAcero <pj.acero@alumnos.upm.es>
2025-11-05 15:34:24 +0100

    FEAT: Test con barrera, punto A y B en Moveit planner, organizacion del paquete general

--- src/Universal_Robots_ROS2_Description/launch/view_ur.launch.py
+++ src/Universal_Robots_ROS2_Description/launch/view_ur.launch.py
@@ -121,7 +121,8 @@ def generate_launch_description():
         [
             PathJoinSubstitution([FindExecutable(name="xacro")]),
             " ",
-            PathJoinSubstitution([FindPackageShare(description_package), "urdf", description_file]),
+            PathJoinSubstitution(
+                [FindPackageShare(description_package), "urdf", description_file]),
             " ",
             "safety_limits:=",
             safety_limits,
